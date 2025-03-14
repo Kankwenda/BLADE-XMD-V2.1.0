10cea66550e17979b047db17cd58cdeca29f9022
Mr blade hassan <Augustinbusime118@gmail.com>
2025-03-15 00:41:38 +0200

    Update groupe.js

--- commandes/groupe.js
+++ commandes/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *𝐀𝐍𝐘𝐖𝐀𝐘-𝐌𝐃*𝕋𝔸𝔾* 🌟
+        🌟 *𝔹𝕃𝔸𝔻𝔼-𝕏𝕄𝔻*𝕋𝔸𝔾* 🌟
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
